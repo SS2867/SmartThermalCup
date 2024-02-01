487d7e2aa1117d29100969b172288c652539f3b4
SS2867 <songzl2003@gmail.com>
2024-02-01 15:29:15 +0800

    Changed to 7-segment 3digit 2531BS

--- SmartThermalCup.ino
+++ SmartThermalCup.ino
@@ -1,4 +1,12 @@
 int RELAY_PIN = 9;
+
+int sevenSegmentIdx = 0;
+int sevenSegmentDisplay[3] = {7, 11, 12};
+int temperatureList[10] = {1023,1023,1023,1023,1023,1023,1023,1023,1023,1023};
+float avgTemperature = 0.;
+int heat = 0;
+int targetTemperature = 28;
+
 void setup() {
   // put your setup code here, to run once:
   noInterrupts();
@@ -8,20 +16,32 @@ void setup() {
 
   
   TIMSK2 = (1<<OCIE2A);
-  TCCR2A |= (1 << CS12)|(1<<WGM12);
-  OCR2A = 3125 - 1;
+  TCCR2A |= (1 << CS22)| (1 << CS20)|(1<<WGM22);
+  OCR2A = 255 - 1;
   interrupts();
 
+  sevenSegmentInit();
+  
   pinMode(RELAY_PIN, OUTPUT);
   
   Serial.begin(9600);
 
 }
 
-int temperatureList[10] = {1023,1023,1023,1023,1023,1023,1023,1023,1023,1023};
-float avgTemperature = 0.;
-int heat = 0;
-int targetTemperature = 28;
+void sevenSegmentInit(){
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+
 
 ISR(TIMER1_COMPA_vect){
   for(int i=0; i<10; i++){  
@@ -35,7 +55,32 @@ ISR(TIMER1_COMPA_vect){
 }
 
 ISR(TIMER2_COMPA_vect){
-
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(2, HIGH); digitalWrite(3, HIGH); digitalWrite(4, HIGH); digitalWrite(5, HIGH); digitalWrite(6, HIGH); digitalWrite(7, HIGH); digitalWrite(8, HIGH);
+  int disp = sevenSegmentDisplay[sevenSegmentIdx];
+  switch(sevenSegmentIdx){
+    case 2: digitalWrite(10, HIGH); break;
+    case 1: digitalWrite(11, HIGH); break;
+    case 0: digitalWrite(12, HIGH); break;
+  }
+  switch(disp){
+    case 0: digitalWrite(2, 0); digitalWrite(3, 0); digitalWrite(4, HIGH); digitalWrite(5, 0); digitalWrite(6, 0); digitalWrite(7, 0); digitalWrite(8, 0); break;
+    case 1: digitalWrite(2, HIGH); digitalWrite(3, 0); digitalWrite(4, HIGH); digitalWrite(5, HIGH); digitalWrite(6, HIGH); digitalWrite(7, 0); digitalWrite(8, HIGH); break;
+    case 2: digitalWrite(2, 0); digitalWrite(3, 0); digitalWrite(4, 0); digitalWrite(5, 0); digitalWrite(6, 0); digitalWrite(7, HIGH); digitalWrite(8, HIGH); break;
+    case 3: digitalWrite(2, 0); digitalWrite(3, 0); digitalWrite(4, 0); digitalWrite(5, HIGH); digitalWrite(6, 0); digitalWrite(7, 0); digitalWrite(8, HIGH); break;
+    case 4: digitalWrite(2, HIGH); digitalWrite(3, 0); digitalWrite(4, 0); digitalWrite(5, HIGH); digitalWrite(6, HIGH); digitalWrite(7, 0); digitalWrite(8, 0); break;
+    case 5: digitalWrite(2, 0); digitalWrite(3, HIGH); digitalWrite(4, 0); digitalWrite(5, HIGH); digitalWrite(6, 0); digitalWrite(7, 0); digitalWrite(8, 0); break;
+    case 6: digitalWrite(2, 0); digitalWrite(3, HIGH); digitalWrite(4, 0); digitalWrite(5, 0); digitalWrite(6, 0); digitalWrite(7, 0); digitalWrite(8, 0); break;
+    case 7: digitalWrite(2, 0); digitalWrite(3, 0); digitalWrite(4, HIGH); digitalWrite(5, HIGH); digitalWrite(6, HIGH); digitalWrite(7, 0); digitalWrite(8, HIGH); break;
+    case 8: digitalWrite(2, 0); digitalWrite(3, 0); digitalWrite(4, 0); digitalWrite(5, 0); digitalWrite(6, 0); digitalWrite(7, 0); digitalWrite(8, 0); break;
+    case 9: digitalWrite(2, 0); digitalWrite(3, 0); digitalWrite(4, 0); digitalWrite(5, HIGH); digitalWrite(6, 0); digitalWrite(7, 0); digitalWrite(8, 0); break;
+    case 10: digitalWrite(2, HIGH); digitalWrite(3, HIGH); digitalWrite(4, HIGH); digitalWrite(5, HIGH); digitalWrite(6, HIGH); digitalWrite(7, HIGH); digitalWrite(8, HIGH); break;
+    case 11: digitalWrite(2, HIGH); digitalWrite(3, HIGH); digitalWrite(4, HIGH); digitalWrite(5, HIGH); digitalWrite(6, HIGH); digitalWrite(7, HIGH); digitalWrite(8, 0); break;
+    case 12: digitalWrite(2, HIGH); digitalWrite(3, HIGH); digitalWrite(4, HIGH); digitalWrite(5, 0); digitalWrite(6, HIGH); digitalWrite(7, HIGH); digitalWrite(8, HIGH); break;
+  }
+  sevenSegmentIdx = (sevenSegmentIdx+1)%3;
 }
 
 void loop() {
@@ -53,6 +98,7 @@ void loop() {
     digitalWrite(RELAY_PIN, HIGH);
     delay(550);
   }
+
 
   delay(100);
 }
